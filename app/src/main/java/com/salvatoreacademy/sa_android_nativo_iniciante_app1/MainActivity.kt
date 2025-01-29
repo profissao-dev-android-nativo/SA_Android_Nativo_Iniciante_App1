25e2570f3ce4de31367c685e11615de7b5476ea5
Paulo Salvatore <salvatoregames@gmail.com>
2025-01-29 18:39:42 -0300

    Otimização dos imports

--- app/src/main/java/com/salvatoreacademy/sa_android_nativo_iniciante_app1/MainActivity.kt
+++ app/src/main/java/com/salvatoreacademy/sa_android_nativo_iniciante_app1/MainActivity.kt
@@ -1,11 +1,8 @@
 package com.salvatoreacademy.sa_android_nativo_iniciante_app1
 
-import android.content.Intent
 import android.os.Bundle
-import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.cardview.widget.CardView
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.recyclerview.widget.LinearLayoutManager
